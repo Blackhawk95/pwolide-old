13c87416c63fecb8bcec392825367381754d972a
Blackhawk95 <abhijith.newton@gmail.com>
2022-06-12 21:26:48 +0530

    a lot of additions netlify function and stuffs

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,15 +1,35 @@
+const { createClient } = require('@supabase/supabase-js');
+const fetch = require('isomorphic-fetch')
+
+const supabaseUrl = 'https://cjqcnyoufeeevygnkhqy.supabase.co'
+const supabaseKey = process.env.SUPABASE_ANON_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
 exports.handler = async (event, context) => {
 
-    function url_func() {
-        const no_of_images = 2;
-        const img = Math.floor(Math.random() * no_of_images);
-        const img_name = "pd_img" +  String(img) + ".jpg";
-        const url = new URL(img_name, "https://cjqcnyoufeeevygnkhqy.supabase.co/storage/v1/object/public/images/pd_img").href;
-        return url;
+    const no_of_images = 2;
+    const img = Math.floor(Math.random() * no_of_images);
+    const img_name = "pd_img" +  String(img) + ".jpg";
+    //const url = new URL(img_name, "https://cjqcnyoufeeevygnkhqy.supabase.co/storage/v1/object/public/images/pd_img").href;
+    const { data, error } = await supabase.storage.from('images').getPublicUrl(img_name);
+
+    // https://github.com/DavidWells/netlify-functions-workshop/blob/master/lessons-code-complete/use-cases/13-returning-dynamic-images/functions/return-image.js
+    console.log(data['publicURL'])
+    let image
+    if(data){
+        const result = await fetch(data['publicURL'])
+        image = await result.buffer()
     }
+
+    console.log(error)
 
     return {
         statusCode: 200,
-        body: JSON.stringify({ "url": url_func() }),
+        headers: {
+            'Content-type': 'image/jpeg'
+          },
+          body: await image.toString('base64'),
+          isBase64Encoded: true
       };
-  }
+}
