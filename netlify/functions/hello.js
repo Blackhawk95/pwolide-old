85dd0e62f66c9f467e3ab2f1bd046cf9c4c66b6d
Blackhawk95 <abhijith.newton@gmail.com>
2022-06-12 21:35:20 +0530

    fixed the domain name change

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -8,21 +8,19 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 
 exports.handler = async (event, context) => {
 
-    const no_of_images = 2;
+    const no_of_images = 2; // hard coded for now ( can be changed TODO)
     const img = Math.floor(Math.random() * no_of_images);
     const img_name = "pd_img" +  String(img) + ".jpg";
     //const url = new URL(img_name, "https://cjqcnyoufeeevygnkhqy.supabase.co/storage/v1/object/public/images/pd_img").href;
     const { data, error } = await supabase.storage.from('images').getPublicUrl(img_name);
 
     // https://github.com/DavidWells/netlify-functions-workshop/blob/master/lessons-code-complete/use-cases/13-returning-dynamic-images/functions/return-image.js
-    console.log(data['publicURL'])
+
     let image
     if(data){
         const result = await fetch(data['publicURL'])
         image = await result.buffer()
     }
-
-    console.log(error)
 
     return {
         statusCode: 200,
